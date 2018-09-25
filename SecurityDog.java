588e46e81f1aae23a3c704dddfeaeec380a3b713
GueTorres <guevara.torres619@outlook.com>
2018-09-25 16:23:52 -0400

    Update SecurityDog.java

--- SecurityDog.java
+++ SecurityDog.java
@@ -1,5 +1,19 @@
 package oopMore;
 
-public class SecurityDog {
-
+public class SecurityDog extends Dog {
+   
+  public void warn() {
+  System.out.println("GRRR GRRRRRR");  
+  }
+  
+  public void attack() {
+  System.out.println("BITE BITE BITE");
+  }
+  
+  @Override
+  public void askForHelp(){
+      //not doing that...machismo
+      //Never surrender!
+  }
+  
 }
