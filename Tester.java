1280cfb1ac3940630850c531211ef61a88adac76
GueTorres <guevara.torres619@outlook.com>
2018-09-25 17:35:15 -0400

    Create Tester.java

---
+++ Tester.java
@@ -0,0 +1,10 @@
+public class Tester { 
+  public char [] [] arrTTT = new char [3] [3];
+        arrTTT [0][0] = 'x';
+        arrTTT [2][0] = 'o';
+        arrTTT [2][2] = 'x';
+        arrTTT [1][1] = 'o';
+        arrTTT [0][2] = 'x';
+        arrTTT [0][1] = 'o';
+        arrTTT [1][2] = 'x';
+        System.out.println(" x wins!");
