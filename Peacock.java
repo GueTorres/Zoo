ec165ed0ab0273a929902ada4172155c585f8285
GueTorres <guevara.torres619@outlook.com>
2018-09-26 14:54:43 -0400

    Update Peacock.java

--- Peacock.java
+++ Peacock.java
@@ -1,5 +1,40 @@
 package oopMore;
 
-public class Peacock {
+public class Peacock extends Bird{
 
+	
+	public static final double MAX_HEIGHT = 92;
+	public static final double MAX_WEIGHT = 13;
+
+	public Peacock(){
+		super();
+	}
+
+	public Peacock(double ht, double wt){
+		super();
+		setHeight(ht);
+		setWeight(wt);
 }
+	
+	
+	public void setHeight(double ht){
+		if(ht > MAX_HEIGHT){
+			//we can throw an exception... or just set it the max
+			super.setHeight(MAX_HEIGHT);//set it to max height val... but not greater
+		}
+		else{
+			super.setHeight(ht);//set the value
+		}
+	}
+	@Override
+	public void setWeight(double wt){
+		if(wt > MAX_WEIGHT){
+			//we can throw an exception... or just set it the max
+			super.setHeight(MAX_WEIGHT);//set it to max weight val... but not greater
+		}
+		else{
+			super.setWeight(wt);//set the value
+		}
+}
+
+
