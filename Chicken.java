2c2d6f5031bad9240eff9b81594e278aaa7096e2
GueTorres <guevara.torres619@outlook.com>
2018-09-26 14:43:18 -0400

    Update Chicken.java

--- Chicken.java
+++ Chicken.java
@@ -1,6 +1,42 @@
 package oopMore;
 
-public class Chicken {
+public class Chicken extends Bird {
+	
+	
+	public static final double MAX_HEIGHT = 26;
+	public static final double MAX_WEIGHT = 8.6;
+
+	public LapDog(){
+		super();
+	}
+
+	public LapDog(double ht, double wt){
+		super();
+		setHeight(ht);
+		setWeight(wt);
+}
+	
+	
+	
+	public void setHeight(double ht){
+		if(ht > MAX_HEIGHT){
+			//we can throw an exception... or just set it the max
+			super.setHeight(MAX_HEIGHT);//set it to max height val... but not greater
+		}
+		else{
+			super.setHeight(ht);//set the value
+		}
+	}
+	@Override
+	public void setWeight(double wt){
+		if(wt > MAX_WEIGHT){
+			//we can throw an exception... or just set it the max
+			super.setHeight(MAX_WEIGHT);//set it to max weight val... but not greater
+		}
+		else{
+			super.setWeight(wt);//set the value
+		}
+}
 	
 	
 
